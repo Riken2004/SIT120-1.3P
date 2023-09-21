a8385dd82add33fe59e5fe5f6dcf947e8ed83731
Riken2004 <139823114+Riken2004@users.noreply.github.com>
2023-09-21 15:53:35 +1000

    Add files via upload

---
+++ views/AboutView.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <div class="header">
+      <h1>My Portfolio</h1>
+    </div>
+    <div class="container">
+      <div class="section">
+        <h2>Work Experience</h2>
+        <ul>
+          <li v-for="experience in workExperience" :key="experience.id">
+            <h3>{{ experience.position }}</h3>
+            <p>{{ experience.description }}</p>
+          </li>
+        </ul>
+      </div>
+      <div class="section">
+        <h2>Education</h2>
+        <ul>
+          <li v-for="education in educationHistory" :key="education.id">
+            <h3>{{ education.degree }}</h3>
+            <p>{{ education.university }}</p>
+            <p>{{ education.date }}</p>
+            <p>{{ education.description }}</p>
+          </li>
+        </ul>
+      </div>
+      <div class="section">
+        <h2>Sports</h2>
+        <ul>
+          <li v-for="sport in sports" :key="sport.id">
+            <h3>{{ sport.name }}</h3>
+            <p>{{ sport.description }}</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      workExperience: [
+        {
+          id: 1,
+          position: 'Fresher',
+          description: 'Designed and managed websites using updated technologies.',
+        },
+      ],
+      educationHistory: [
+        {
+          id: 1,
+          degree: 'Bachelor of Information Technology',
+          university: 'Deakin University',
+          date: '2023 - 2026',
+          description: 'I am learning about computer programming and focusing on creating software programs.',
+        },
+      ],
+      sports: [
+        {
+          id: 1,
+          name: 'Cricket',
+          description: 'Cricket is one of the most followed game in the world.',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* CSS for Sections */
+.section {
+  margin: 20px 0;
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.section h2 {
+  font-family: 'Pacifico', cursive;
+  color: #333;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 20px 0;
+  padding: 10px;
+  background-color: #f9f9f9;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+}
+
+li h3 {
+  font-family: 'Roboto', sans-serif;
+  color: #333;
+}
+
+/* Additional Styling for Education and Sports Sections */
+.education-section,
+.sports-section {
+  background-color: #f0f0f0;
+}
+
+/* Responsiveness */
+@media (max-width: 768px) {
+  .section {
+    padding: 10px;
+    margin: 10px 0;
+  }
+  li {
+    padding: 5px;
+    margin: 10px 0;
+  }
+}
+</style>
---
+++ views/new project.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="contact-form">
+      <h1 style="background-color: yellowgreen;">Contact Form</h1>
+      <form @submit.prevent="submitForm">
+        <label for="Name">Name:</label>
+        <input id="Name" v-model="formData.name" required placeholder="Please let me know you name">
+        
+        <label for="contact">Contact:</label>
+        <input id="contact" v-model="formData.contact" type="Contact" required placeholder="Your Contact no">
+    
+        <label for="message">Message:</label>
+        <textarea id="message" v-model="formData.message" rows="4" required placeholder="Drop a message for me"></textarea>
+    
+        <button type="submit" class="submit-button">Submit</button>
+      </form>
+    </div>
+    
+    </template>
+    
+    <script>
+    export default {
+    data() {
+      return {
+        formData: {
+          Name: '',
+          contact: '',
+          message: ''
+        }
+      };  
+    },
+    methods: {
+      submitForm() {
+        console.log('Form Data:', this.formData);
+        alert('Form submitted successfully!');
+      }
+    }
+    }
+    </script>
+    
+    
+    
+<style>
+    .contact-form {
+      background-color: #f8f8f8;
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+      text-align: center;
+    }
+    
+    .contact-form h1 {
+      background-color: yellowgreen;
+      color: #fff;
+      padding: 10px;
+      border-radius: 5px;
+    }
+    
+    .contact-form form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 15px;
+    }
+    
+    .contact-form label {
+      font-weight: bold;
+    }
+    
+    .contact-form input[type="text"],
+    .contact-form input[type="Contact"],
+    .contact-form textarea {
+      width: 100%;
+      padding: 10px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+    }
+    
+    .contact-form textarea {
+      resize: vertical;
+    }
+    
+    .contact-form .submit-button {
+      background-color: yellowgreen;
+      color: #fff;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      transition: background-color 0.3s;
+    }
+    
+    .contact-form .submit-button:hover {
+      background-color: #4CAF50;
+    }
+    
+    /* Responsiveness */
+    
+    @media screen and (max-width: 600px) {
+      .contact-form form {
+        width: 80%;
+      }
+    }
+</style> 
